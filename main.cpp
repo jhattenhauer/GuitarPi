24d1f2c4e779f84ab619f8c79ce0cef2042028f0
James Hattenhauer <jhattenhauer@outlook.com>
2025-11-12 11:37:34 -0330

    updated main

--- main.cpp
+++ main.cpp
@@ -10,6 +10,8 @@ int main() {
         while(true){
             std::cout << "Starting Recording..." << std::endl;
             std::vector<int32_t> sample = CaptureSample();
+            std::vector<float> floatSample = Int_to_Float(sample);
+            apply_effects(floatSample);
         }
     UnInit();
     return 0;
