e0fa43c1d604ea406bcfef5f185be8ab35dd5c5c
jimmy <jhattenhauer@outlook.com>
2025-11-17 21:27:58 -0330

    pass alsa endpoint as caputre device

--- main.cpp
+++ main.cpp
@@ -5,10 +5,15 @@
 #include "libraries/alsa_utils.h"
 #include "libraries/apply_effects.h"
 
-int main() {
+int main(int argc, char* argv[]) {
+
+    char* alsa_endpoint = NULL; //pass argument alsa endpoint ex: "hw:3,0"
+    if (argc != 0){
+    alsa_endpoint = argv[1];
+    }
     if (InitCapture(NULL)){
+        std::cout << "Starting Recording..." << std::endl;
         while(true){
-            std::cout << "Starting Recording..." << std::endl;
             std::vector<int32_t> sample = CaptureSample();
             std::vector<float> floatSample = Int_to_Float(sample);
             apply_effects(floatSample);
