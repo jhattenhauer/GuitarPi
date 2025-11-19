dfef835600f25c58733132b3b8f039655c1368df
James Hattenhauer <jhattenhauer@outlook.com>
2025-11-19 09:38:08 -0330

    added settings update loop

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include "libraries/alsa_utils.h"
 #include "libraries/apply_effects.h"
 
-int func(int argc, char* argv[]) {
+int main(int argc, char* argv[]) {
 
     char* capture_endpoint = NULL; //pass argument alsa endpoint ex: "hw:3,0"
     char* playback_endpoint = NULL; //pass argument alsa endpoint ex: "hw:1,0"
@@ -20,11 +20,19 @@ int func(int argc, char* argv[]) {
     try {playback_endpoint = argv[2];} catch (int e) {std::cerr << "Pass playback point as 'hw:1,0'" << std::endl;}
     }
 
+    int update_settings_count = 0;
+
     if (InitCapture(capture_endpoint)){
         std::cout << "Starting Recording..." << std::endl;
         if (InitPlayback(playback_endpoint)){
             std::cout << "Starting Playback..." << std::endl;
             while(true){
+                update_settings_count += 1;
+                if (update_settings_count == 1000) {
+                    read_settings();
+                    update_settings_count = 0;
+                }
+
                 std::vector<int32_t> sample = CaptureSample();
                 std::vector<float> floatSample = Int_to_Float(sample);
                 apply_effects(floatSample);
@@ -36,8 +44,4 @@ int func(int argc, char* argv[]) {
     UnInitCaptureDevice();
     }
     return 0;
-}
-
-int main(){
-    read_settings();
 }
